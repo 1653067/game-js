daf2e83293bf321d63dbe68bbaccf7c395b31aa5
tranphucbol <tranphucbol@gmail.com>
2019-02-17 14:51:00 +0700

    fix

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const bhs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/terris', (req, res) => {
-    res.sendFile(__dirname + '/public/terris/index.html');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('terris/index');
 });
 
-const port = process.argv[2] || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log('started');
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
